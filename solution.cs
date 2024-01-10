dad078822c827774c54423f7ec80dd03b2b131cc
Kooreis <kooreis.official@gmail.com>
2024-01-10 08:08:01 -0600

    This is the main function that finds the maximum XOR of two numbers in an array. It uses a mask to isolate bits from the most significant bit down to the least significant bit. It then uses a set to store the prefixes of the numbers in the array. It checks if the XOR of the temporary maximum and the prefix exists in the set. If it does, it updates the maximum.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,3 @@
-Here is a JavaScript solution for the problem:
-
-```javascript
 function findMaxXOR(arr) {
     let max = 0, mask = 0;
     for (let i = 31; i >= 0; i--) {
@@ -19,9 +16,3 @@ function findMaxXOR(arr) {
     }
     return max;
 }
-
-let arr = [3, 10, 5, 25, 2, 8];
-console.log(findMaxXOR(arr));
-```
-
-This console application can be run in a Node.js environment. It finds the maximum XOR of two numbers in an array. The `findMaxXOR` function takes an array of numbers as input and returns the maximum XOR value. The array `[3, 10, 5, 25, 2, 8]` is used as an example here, and the result will be printed to the console.
