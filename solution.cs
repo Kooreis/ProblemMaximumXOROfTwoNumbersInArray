e79f225f79b01f9214d3b46144a80d3023981d12
Kooreis <kooreis.official@gmail.com>
2024-01-11 08:08:01 -0600

    This is the initialization of the array that will be used as input for the findMaxXOR function. The array contains the numbers 3, 10, 5, 25, 2, and 8.

--- solution.cs
+++ solution.cs
@@ -1,18 +1,1 @@
-function findMaxXOR(arr) {
-    let max = 0, mask = 0;
-    for (let i = 31; i >= 0; i--) {
-        mask = mask | (1 << i);
-        let set = new Set();
-        for (let num of arr) {
-            set.add(num & mask);
-        }
-        let temp = max | (1 << i);
-        for (let prefix of set) {
-            if (set.has(temp ^ prefix)) {
-                max = temp;
-                break;
-            }
-        }
-    }
-    return max;
-}
+let arr = [3, 10, 5, 25, 2, 8];
